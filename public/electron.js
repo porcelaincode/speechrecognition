f959e0fa02105bbec45449de0126c1f6fb3f3772
Vatsal Pandya <67172122+porcelaincode@users.noreply.github.com>
2021-02-15 23:47:47 +0530

    Minor UI Change

--- public/electron.js
+++ public/electron.js
@@ -4,8 +4,8 @@ const isDev = require("electron-is-dev")
 
 function createWindow () {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1000,
+    height: 700,
     webPreferences: {
             nodeIntegration: true
         }
